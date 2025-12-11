5bfc543e157e3f5cb69a76b51f18f47d038e16c7
Omar <omar.garcia@morsa.com.mx>
2025-12-11 13:40:07 -0600

    Creando el FORM para definir el PRESUPUESTO

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { useState } from "react";
 
 export default function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    console.log(e.target.valueAsNumber);
+  };
+
   return (
-    <div>
-      
-    </div>
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">Definir Presupuesto</label>
+
+        <input
+          type="number"
+          name="budget"
+          id="budget"
+          placeholder="Define tu presupuesto"
+          className="w-full bg-white border border-gray-200 p-2"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+
+      <input
+        type="submit"
+        value='Definir Presupuesto'
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+      />
+    </form>
   )
 };
