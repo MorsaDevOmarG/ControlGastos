c3d1483ce735f01064f38d09a2c118d86848b94b
Omar <omar.garcia@morsa.com.mx>
2025-12-11 17:17:39 -0600

    Agregando el CONTEXT a nuestra APP

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,28 +1,36 @@
-import { createContext, useReducer, Dispatch } from "react";
+import { createContext, useReducer, Dispatch, ReactNode } from "react";
 import {
   budgetReducer,
   initialState,
   BudgetActions,
   BudgetState,
 } from "../reducers/budget-reducer";
+import { triggerAsyncId } from "async_hooks";
 
 type BudgetContextProps = {
   state: BudgetState;
   dispatch: Dispatch<BudgetActions>;
 };
 
+type BudgetProviderProps = {
+  children: ReactNode
+};
+
 // export const BudgetContext = createContext<BudgetContextProps>({} as BudgetContextProps);
 export const BudgetContext = createContext<BudgetContextProps>(null!);
 
 // De donde vienen los datos, children hace referencia a los hijos de un COMPONENTE
-export const BudgetProvider = ({ children }) => {
+export const BudgetProvider = ({ children } : BudgetProviderProps) => {
   const [state, dispatch] = useReducer(budgetReducer, initialState);
+
+  // const auth = true;
 
   return (
     <BudgetContext.Provider
       value={{
         state,
         dispatch,
+        // auth
       }}
     >
       {children}
