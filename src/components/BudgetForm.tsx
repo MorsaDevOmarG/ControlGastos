d85033b9fec24455a2c17b645f93576e17f792ae
Omar <omar.garcia@morsa.com.mx>
2025-12-11 13:48:56 -0600

    Validando el PRESUPUESTO

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function BudgetForm() {
   const [budget, setBudget] = useState(0);
 
   const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-    console.log(e.target.valueAsNumber);
+    // console.log(e.target.value);
+    // console.log(e.target.valueAsNumber);
 
     setBudget(e.target.valueAsNumber);
   };
+
+  const isValid = useMemo(() => {
+    // console.log(budget);
+
+    return isNaN(budget) || budget <= 0;
+  }, [budget]);
 
   return (
     <form className="space-y-5">
@@ -29,7 +35,8 @@ export default function BudgetForm() {
       <input
         type="submit"
         value='Definir Presupuesto'
-        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40 disabled:cursor-default"
+        disabled={isValid}
       />
     </form>
   )
