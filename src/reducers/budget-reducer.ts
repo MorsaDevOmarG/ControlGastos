81efde45ee42ed9f6b827d305c97547ecb316962
Omar <omar.garcia@morsa.com.mx>
2025-12-12 13:12:56 -0600

    Mostrando el MODAL cuando agregamos un nuevo PRESUPUESTO

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,12 +1,15 @@
 export type BudgetActions =
-  { type: 'add-bugdet', payload: { budget: number } };
+  { type: 'add-bugdet', payload: { budget: number } } |
+  { type: 'show-modal' }
 
 export type BudgetState = {
-  budget: number
+  budget: number;
+  modal: boolean;
 };
 
 export const initialState: BudgetState = {
-  budget: 0
+  budget: 0,
+  modal: false
 };
 
 export const budgetReducer = (
@@ -18,6 +21,14 @@ export const budgetReducer = (
     return {
       ...state,
       budget: action.payload.budget
+    };
+  }
+
+  if (action.type === 'show-modal') {
+
+    return {
+      ...state,
+      modal: true
     };
   }
 
